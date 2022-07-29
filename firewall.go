2dcd1c28d7da3842102ab02b8cdea52dc3ef95b4
Bryan MacLean <bryan.maclean@cohesive.net>
2022-07-28 21:32:14 -0300

    Modify JSON to return a collection of rules

--- firewall.go
+++ firewall.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func (c *Client) GetFirewallRules() (FirewallResponse, error) {
@@ -18,8 +19,22 @@ func (c *Client) GetFirewallRules() (FirewallResponse, error) {
 		log.Println(err)
 	}
 
+	var result map[string]interface{}
+	json.Unmarshal([]byte(body), &result)
+	var rules = result["response"].([]interface{})
+	var list string = "{\"response\" : [ "
+	for _, rule := range rules {
+		list += "{ "
+		values := rule.([]interface{})
+		desc := values[0].(string)
+		id := strconv.Itoa(int(values[1].(float64)))
+		list += "\"id\":\"" + id + "\", \"rule\":\"" + desc + "\"" + " },"
+	}
+	list = list[:len(list)-1]
+	list += "]}"
+
 	firewallResponse := FirewallResponse{}
-	errUnmarshal := json.Unmarshal([]byte(body), &firewallResponse)
+	errUnmarshal := json.Unmarshal([]byte(list), &firewallResponse)
 
 	if err != nil {
 		log.Println(errUnmarshal)
@@ -27,3 +42,4 @@ func (c *Client) GetFirewallRules() (FirewallResponse, error) {
 
 	return firewallResponse, nil
 }
+
