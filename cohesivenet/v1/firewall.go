8fb4f59c4ba61df22fccae65072c6c2586fe232c
hkf1977 <satanswell@hotmail.co.uk>
2022-09-20 07:29:29 +0100

    handle maps of routes and rules

--- cohesivenet/v1/firewall.go
+++ cohesivenet/v1/firewall.go
@@ -32,19 +32,29 @@ func (c *Client) GetFirewallRules() (FirewallResponse, error) {
 	return firewallResponse, nil
 }
 
-func (c *Client) CreateFirewallRules(rule *FirewallRule) (FirewallResponse, error) {
-	rb, err := json.Marshal(rule)
-	if err != nil {
-		log.Println(err)
-	}
-	req, err := http.NewRequest("POST", fmt.Sprintf("%s/firewall/rules", c.HostURL), strings.NewReader(string(rb)))
-	if err != nil {
-		log.Println(err)
-	}
+func (c *Client) CreateFirewallRules(ruleList []*FirewallRule) ([]*FirewallResponse, error) {
+	i := 0
+	for _, rl := range ruleList {
+		rule := FirewallRule{
+			ID:   rl.ID,
+			Rule: rl.Rule,
+		}
 
-	_, err2 := c.doRequest(req)
-	if err != nil {
-		log.Println(err2)
+		rb, err := json.Marshal(rule)
+		if err != nil {
+			log.Println(err)
+		}
+		req, err := http.NewRequest("POST", fmt.Sprintf("%s/firewall/rules", c.HostURL), strings.NewReader(string(rb)))
+		if err != nil {
+			log.Println(err)
+		}
+
+		_, err2 := c.doRequest(req)
+		if err != nil {
+			log.Println(err2)
+		}
+
+		i++
 	}
 
 	req2, err := http.NewRequest("GET", fmt.Sprintf("%s/firewall/rules", c.HostURL), nil)
@@ -57,37 +67,66 @@ func (c *Client) CreateFirewallRules(rule *FirewallRule) (FirewallResponse, erro
 		log.Println(err)
 	}
 
-	simpleJson := simplifyJson(string(body))
-	newRules := FirewallResponse{}
-	errUnmarshal := json.Unmarshal([]byte(simpleJson), &newRules)
+	//simpleJson := simplifyJson(string(body))
+	//newRules := FirewallResponse{}
+	//errUnmarshal := json.Unmarshal([]byte(simpleJson), &newRules)
+
+	//if err != nil {
+	//	log.Println(errUnmarshal)
+	//}
+
+	//returnRules := FirewallResponse{}
+	//for _, r := range newRules.FirewallRules {
+	//			if strings.EqualFold(r.Rule, rule.Rule) {
+	//		returnRules.FirewallRules = []FirewallRule{r}
+	//
+	//	}
+	//}
+	returnRules := []*FirewallResponse{}
+	errUnmarshal := json.Unmarshal([]byte(body), &returnRules)
 	if err != nil {
 		log.Println(errUnmarshal)
-	}
-
-	returnRules := FirewallResponse{}
-	for _, r := range newRules.FirewallRules {
-		if strings.EqualFold(r.Rule, rule.Rule) {
-			returnRules.FirewallRules = []FirewallRule{r}
-
-		}
 	}
 
 	return returnRules, nil
 }
 
-func (c *Client) DeleteRule(ruleId string) error {
-	req, err := http.NewRequest("DELETE", fmt.Sprintf("%s/firewall/rules/%s", c.HostURL, ruleId), nil)
+func (c *Client) DeleteRule() error {
+	req1, err := http.NewRequest("GET", fmt.Sprintf("%s/firewall/rules", c.HostURL), nil)
 	if err != nil {
-		return err
+		log.Println(err)
 	}
 
-	body, err := c.doRequest(req)
+	rules, err := c.doRequest(req1)
 	if err != nil {
-		return err
+		log.Println(err)
 	}
 
-	if string(body) == "Deleted route" {
-		return err
+	simpleJson := simplifyJson(string(rules))
+
+	firewallResponse := FirewallResponse{}
+	errUnmarshal := json.Unmarshal([]byte(simpleJson), &firewallResponse)
+
+	if err != nil {
+		log.Println(errUnmarshal)
+	}
+
+	for _, r := range firewallResponse.FirewallRules {
+		ruleId := r.ID
+
+		req, err := http.NewRequest("DELETE", fmt.Sprintf("%s/firewall/rules/%s", c.HostURL, ruleId), nil)
+		if err != nil {
+			return err
+		}
+
+		body, err := c.doRequest(req)
+		if err != nil {
+			return err
+		}
+
+		if string(body) == "Deleted route" {
+			return err
+		}
 	}
 
 	return nil
--- cohesivenet/v1/routes.go
+++ cohesivenet/v1/routes.go
@@ -57,50 +57,92 @@ func (c *Client) GetRoutes() (RouteResponse, error) {
 	return routeResponse, nil
 }
 
-func (c *Client) CreateRoute(route *Route) (RouteResponse, error) {
+func (c *Client) CreateRoute(routeList []*Route) ([]*RouteResponse, error) {
+	i := 0
+	for _, rt := range routeList {
+		route := Route{
+			Cidr:        rt.Cidr,
+			Description: rt.Description,
+			Interface:   rt.Interface,
+			Gateway:     rt.Gateway,
+			Tunnel:      rt.Tunnel,
+			Advertise:   rt.Advertise,
+			Metric:      rt.Metric,
+		}
+		rb, err := json.Marshal(route)
+		if err != nil {
+			log.Println(err)
+		}
+		req, err := http.NewRequest("POST", fmt.Sprintf("%s/routes", c.HostURL), strings.NewReader(string(rb)))
+		if err != nil {
+			log.Println(err)
+		}
+		_, err2 := c.doRequest(req)
+		if err2 != nil {
+			log.Println(err)
+		}
 
-	rb, err := json.Marshal(route)
+		i++
+	}
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/routes", c.HostURL), nil)
 	if err != nil {
 		log.Println(err)
 	}
-	req, err := http.NewRequest("POST", fmt.Sprintf("%s/routes", c.HostURL), strings.NewReader(string(rb)))
-
-	if err != nil {
-		log.Println(err)
-	}
-
 	body, err := c.doRequest(req)
 	if err != nil {
 		log.Println(err)
 	}
 
-	simpleJson := SimplifyJson(string(body))
-
-	newRoute := RouteResponse{}
-	errUnmarshal := json.Unmarshal([]byte(simpleJson), &newRoute)
+	newRoute := []*RouteResponse{}
+	//errUnmarshal := json.Unmarshal([]byte(simpleJson), &newRoute)
+	errUnmarshal := json.Unmarshal([]byte(body), &newRoute)
 	if err != nil {
 		log.Println(errUnmarshal)
 	}
 
 	return newRoute, err
+
 }
 
-func (c *Client) DeleteRoute(routeId string) error {
-	req, err := http.NewRequest("DELETE", fmt.Sprintf("%s/routes/%s", c.HostURL, routeId), nil)
+func (c *Client) DeleteRoute() error {
+
+	req1, err := http.NewRequest("GET", fmt.Sprintf("%s/routes", c.HostURL), nil)
 	if err != nil {
-		return err
+		log.Println(err)
 	}
-
-	body, err := c.doRequest(req)
+	routes, err := c.doRequest(req1)
 	if err != nil {
-		return err
+		log.Println(err)
 	}
 
-	if string(body) == "Deleted route" {
-		return err
+	simpleJson := SimplifyJson(string(routes))
+	routeResponse := RouteResponse{}
+	errUnmarshal := json.Unmarshal([]byte(simpleJson), &routeResponse)
+	if err != nil {
+		log.Println(errUnmarshal)
 	}
 
+	for _, r := range routeResponse.Routes {
+		routeId := r.ID
+
+		req, err := http.NewRequest("DELETE", fmt.Sprintf("%s/routes/%s", c.HostURL, routeId), nil)
+		if err != nil {
+			return err
+		}
+
+		body, err := c.doRequest(req)
+		if err != nil {
+			return err
+		}
+
+		if string(body) == "Deleted route" {
+			return err
+		}
+
+	}
 	return nil
+
 }
 
 func SimplifyJson(input string) string {
