412e5423024dc30a5cb14a383812fb741403ee20
bplatta <bmplatta@gmail.com>
2022-09-27 14:56:04 -0500

    Add overlay networks API

--- cohesivenet/api_firewall.go
+++ cohesivenet/api_firewall.go
@@ -4794,3 +4794,143 @@ func (api *FirewallApiService) PutUpdateFirewallSubtable(r ApiPutUpdateFirewallS
 
 // 	return localVarHTTPResponse, nil
 // }
+
+
+// type ApiExportFirewallRulesRequest struct {
+// 	ctx context.Context
+// 	ApiService *OverlayNetworkApiService
+// 	group *string
+// 	tables *string
+// 	type_ *string
+// }
+
+// // Export group only
+// func (r ApiExportFirewallRulesRequest) Group(group string) ApiExportFirewallRulesRequest {
+// 	r.group = &group
+// 	return r
+// }
+
+// // Tables to export. Accepts csv. Defaults to all.
+// func (r ApiExportFirewallRulesRequest) Tables(tables string) ApiExportFirewallRulesRequest {
+// 	r.tables = &tables
+// 	return r
+// }
+
+// // One of json or plaintext
+// func (r ApiExportFirewallRulesRequest) Type_(type_ string) ApiExportFirewallRulesRequest {
+// 	r.type_ = &type_
+// 	return r
+// }
+
+// func (r ApiExportFirewallRulesRequest) Execute() (**os.File, *http.Response, error) {
+// 	return r.ApiService.ExportFirewallRulesExecute(r)
+// }
+
+// /*
+// ExportFirewallRules V2 Export firewall rules
+
+// Download export of firewall rules
+
+//  @param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+//  @return ApiExportFirewallRulesRequest
+// */
+// func (a *OverlayNetworkApiService) ExportFirewallRules(ctx context.Context) ApiExportFirewallRulesRequest {
+// 	return ApiExportFirewallRulesRequest{
+// 		ApiService: a,
+// 		ctx: ctx,
+// 	}
+// }
+
+// // Execute executes the request
+// //  @return *os.File
+// func (a *OverlayNetworkApiService) ExportFirewallRulesExecute(r ApiExportFirewallRulesRequest) (**os.File, *http.Response, error) {
+// 	var (
+// 		localVarHTTPMethod   = http.MethodGet
+// 		localVarPostBody     interface{}
+// 		formFiles            []formFile
+// 		localVarReturnValue  **os.File
+// 	)
+
+// 	localBasePath, err := a.client.cfg.ServerURLWithContext(r.ctx, "OverlayNetworkApiService.ExportFirewallRules")
+// 	if err != nil {
+// 		return localVarReturnValue, nil, &GenericAPIError{error: err.Error()}
+// 	}
+
+// 	localVarPath := localBasePath + "/v2/firewall/rules/export"
+
+// 	localVarHeaderParams := make(map[string]string)
+// 	localVarQueryParams := url.Values{}
+// 	localVarFormParams := url.Values{}
+
+// 	if r.group != nil {
+// 		localVarQueryParams.Add("group", parameterToString(*r.group, ""))
+// 	}
+// 	if r.tables != nil {
+// 		localVarQueryParams.Add("tables", parameterToString(*r.tables, ""))
+// 	}
+// 	if r.type_ != nil {
+// 		localVarQueryParams.Add("type", parameterToString(*r.type_, ""))
+// 	}
+// 	// to determine the Content-Type header
+// 	localVarHTTPContentTypes := []string{}
+
+// 	// set Content-Type header
+// 	localVarHTTPContentType := selectHeaderContentType(localVarHTTPContentTypes)
+// 	if localVarHTTPContentType != "" {
+// 		localVarHeaderParams["Content-Type"] = localVarHTTPContentType
+// 	}
+
+// 	// to determine the Accept header
+// 	localVarHTTPHeaderAccepts := []string{"text/plain", "application/json"}
+
+// 	// set Accept header
+// 	localVarHTTPHeaderAccept := selectHeaderAccept(localVarHTTPHeaderAccepts)
+// 	if localVarHTTPHeaderAccept != "" {
+// 		localVarHeaderParams["Accept"] = localVarHTTPHeaderAccept
+// 	}
+// 	req, err := a.client.prepareRequest(r.ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, formFiles)
+// 	if err != nil {
+// 		return localVarReturnValue, nil, err
+// 	}
+
+// 	localVarHTTPResponse, err := a.client.callAPI(req)
+// 	if err != nil || localVarHTTPResponse == nil {
+// 		return localVarReturnValue, localVarHTTPResponse, err
+// 	}
+
+// 	localVarBody, err := ioutil.ReadAll(localVarHTTPResponse.Body)
+// 	localVarHTTPResponse.Body.Close()
+// 	localVarHTTPResponse.Body = ioutil.NopCloser(bytes.NewBuffer(localVarBody))
+// 	if err != nil {
+// 		return localVarReturnValue, localVarHTTPResponse, err
+// 	}
+
+// 	if localVarHTTPResponse.StatusCode >= 300 {
+// 		newErr := &GenericAPIError{
+// 			body:  localVarBody,
+// 			error: localVarHTTPResponse.Status,
+// 		}
+// 		if localVarHTTPResponse.StatusCode == 403 {
+// 			var v ErrorResponse
+// 			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+// 			if err != nil {
+// 				newErr.error = err.Error()
+// 				return localVarReturnValue, localVarHTTPResponse, newErr
+// 			}
+// 			newErr.model = v
+// 		}
+// 		return localVarReturnValue, localVarHTTPResponse, newErr
+// 	}
+
+// 	err = a.client.decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+// 	if err != nil {
+// 		newErr := &GenericAPIError{
+// 			body:  localVarBody,
+// 			error: err.Error(),
+// 		}
+// 		return localVarReturnValue, localVarHTTPResponse, newErr
+// 	}
+
+// 	return localVarReturnValue, localVarHTTPResponse, nil
+// }
+
